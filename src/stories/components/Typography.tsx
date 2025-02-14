dab18d0f299a75bbfce5a08842f6da0670d343a5
luancardosoti <luancardosoti@gmail.com>
2025-02-14 04:00:19 -0300

    teste sem tailwindcss correto unico arquivo

--- src/stories/components/Typography.tsx
+++ src/stories/components/Typography.tsx
@@ -2,8 +2,13 @@ import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import type { VariantProps } from 'class-variance-authority'
 import { cva } from 'class-variance-authority'
-import { cn } from '../../lib/utils'
 
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 const typographyVariants = cva(
   'font-normal',
   {
